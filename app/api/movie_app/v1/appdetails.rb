9930e8f50f91acfc04583259a83b8831c5742b47
Aditya220697 <adityasingh.singh421@gmail.com>
2023-11-30 18:13:01 +0530

    user and appopen fix

--- app/api/movie_app/v1/appdetails.rb
+++ app/api/movie_app/v1/appdetails.rb
@@ -176,6 +176,229 @@ module MovieApp
                   end
                 end
             end
+
+
+            # resource :detailPage do
+            #     desc "Match List on Home API"
+            #     before {api_params}
+            #     params do
+            #       # requires :userId, type: String, allow_blank: false
+            #       # requires :securityToken, type: String, allow_blank: false
+            #       # requires :versionName, type: String, allow_blank: false
+            #       # requires :versionCode, type: String, allow_blank: false
+            #       requires :contentId, type: String, allow_blank: false
+            #     end
+            #     post do
+            #       begin
+            #         # user = valid_user(params['userId'].to_i, params['securityToken'])
+            #         if true
+            #           episodeList=[]
+            #           contentList=[]
+            #           l=Content.find_by_id(params[:contentId].to_i)
+            #           l.episodes.each do |element|
+            #             episodeHash={id: element.id,title: element.thumbnail}
+            #             episodeList << episodeHash
+            #           end
+            #           c=Content.order(views: :desc).limit(10)
+            #         #   order("RANDOM(1,3)").limit(10)
+            #           c.each do |item|
+            #             contentHash = {id: item.id, banner: item.banner}
+            #             contentList << contentHash
+            #           end
+            #           {
+            #             message: "MSG_SUCCESS", 
+            #             status: 200, 
+            #             url: l.episodes.first.url,
+            #             episodeList: episodeList,
+            #             contentList: contentList
+            #           }
+            #         else
+            #           {message: "INVALID_USER", status: 500}
+            #         end  
+            #       rescue Exception => e
+            #         logger.info "API Exception-#{Time.now}-subscriptionList-#{params.inspect}-Error-#{e}"
+            #         {message: "MSG_ERROR", status: 500}
+            #       end
+            #     end
+            # end
+
+            resource :detailPage do
+
+                desc "Detail Page on Home API"
+                before {api_params}
+        
+                params do
+                  requires :userId, type: String, allow_blank: false
+                  # requires :securityToken, type: String, allow_blank: false
+                  # requires :versionName, type: String, allow_blank: false
+                  # requires :versionCode, type: String, allow_blank: false
+                  requires :isEpisode, type: String, allow_blank: false
+                  optional :contentId, type: String, allow_blank: false
+                  optional :episodeId, type: String, allow_blank: false
+                end
+        
+                post do
+                  begin
+                    # user = valid_user(params['userId'].to_i, params['securityToken'])
+                    if true
+        
+                      if params[:contentId].present?
+                        cast=[]
+                        # director=[]
+                        episode=[]
+                        recommendation=[]
+                        l=Content.find_by_id(params[:contentId].to_i)
+        
+                        count=l.views.to_i+1
+                        l.update(views: count)
+        
+                        episodeData=Episode.where(content_id: l.id).first
+        
+                        castList=episodeData.cast
+                        directorList=episodeData.director
+        
+                        episodeList=Episode.where(content_id: params[:contentId].to_i)
+        
+                        episodeList.each do |element|
+                          episodeHash={id: element.id, name: element.title, thumbnail: element.thumbnail, runtime: element.runtime, genre: l.genre, vipStatus: element.vip_status }
+                          episode << episodeHash
+                        end
+        
+                        recommendationList=Content.order(views: :desc)
+                        recommendationList.each do |element|
+                          recommendationHash={id: element.id, thumbnail: element.banner, vipStatus: element.vip_status.present? }
+                          recommendation << recommendationHash
+                        end
+                        detailsList = {title: l.title, episodeId: episodeData.id, episodeName: episodeData.title, year: l.release_date, trailerLink: l.trailer_link, genre: l.genre, story: episodeData.story, url: episodeData.url, runtime: episodeData.runtime, castList:l.cast, directors: l.director}
+                        {
+                          message: "MSG_SUCCESS", 
+                          status: 200, 
+                          videoDetails: detailsList,
+                          episodeList: episode,
+                          recommendationList: recommendation
+                        }
+        
+                      else
+        
+                        if episodeList == false
+                          cast=[]
+                          # director=[]
+                          episode=[]
+                          recommendation=[]
+                          episodeData=Episode.find_by_id(params[:episodeId].to_i)
+                          l=Content.find_by_id(episodeData.content_id)
+                          count=l.views.to_i+1
+                          l.update(views: count)
+                          detailsList = {title: l.title, episodeId: episodeData.id, episodeName: episodeData.title, year: l.release_date, trailerLink: l.trailer_link, genre: l.genre, story: episodeData.story, url: episodeData.url, runtime: episodeData.runtime, castList: l.cast, directors: l.director}
+                          {
+                            message: "MSG_SUCCESS", 
+                            status: 200, 
+                            videoDetails: detailsList
+                          }
+                        else
+                          cast=[]
+                          # director=[]
+                          episode=[]
+                          recommendation=[]
+                          episodeData=Episode.find_by_id(params[:episodeId].to_i)
+        
+                              castList=episodeData.cast_id
+                              directorList=episodeData.director_id
+                              
+                              # wh=Watchhistory.find_by(episode_id: params[:episodeId].to_i)
+        
+                              l=Content.find_by_id(episodeData.content_id)
+        
+                              count=l.views.to_i+1
+                              l.update(views: count)
+                              
+                              # if !wh.present?
+                              #   Watchhistory.create(user_id: params[:userId].to_i,content_id: l.id,episode_id: params[:episodeId].to_i)
+                              # else
+                              #   wh.touch
+                              # end
+        
+                              # episodeList
+                              episodeList=Episode.where(content_id: l.id)
+                              episodeList.each do |element|
+                                episodeHash={id: element.id, name: element.title, thumbnail: element.thumbnail, runtime: element.runtime, vipStatus: element.vip_status }
+                                episode << episodeHash
+                              end
+        
+                              recommendationList=Content.where(genre: l.genre)
+                              recommendationList.each do |element|
+                                recommendationHash={id: element.id, thumbnail: element.banner, vipStatus: element.vip_status.present? }
+                                recommendation << recommendationHash
+                              end
+        
+                              castList=castList.split(",")
+                              castList.each do |item|
+                                k= Cast.find_by_id(item.to_i)
+                                castHash = {id: k.id, name: k.name, image_url: k.image}
+                                cast << castHash
+                              end
+        
+                              director=""
+                              directorList=directorList.split(",")
+                              directorList.each_with_index do |item, index|
+                                k = Director.find_by_id(item.to_i)
+                                director << k.name
+                                director << "," unless index == directorList.length - 1  # Add '/' except for the last item
+                              end
+        
+        
+                              genreList=""
+                              genre = l.genre.split(",")
+                              genre.each_with_index do |item, index|
+                                genreList << item
+                                genreList << "/" unless index == directorList.length - 1
+                              end
+        
+                              isFav=false
+                              isWL=false
+                              hisList=History.find_by_user_id(params[:userId].to_i)
+                              if hisList
+                                fList=hisList.favorite_list
+                                wList=hisList.watch_list
+        
+                                fList=fList.split(",")
+                                if fList.include?(episodeData.id.to_s)
+                                  isFav=true
+                                else
+                                  isFav=false
+                                end
+        
+                                wList=wList.split(",")
+                                if wList.include?(episodeData.id.to_s)
+                                  isWL=true
+                                else
+                                  isWL=false
+                                end
+                              end
+        
+                              wh=Watchhistory.find_by("user_id = ? and episode_id = ?",params[:userId].to_i, episodeData.id)
+        
+                              detailsList = {favList: isFav, watchList: isWL, title: l.title, episodeId: episodeData.id, episodeName: episodeData.title, year: l.release_date, trailerLink: l.trailer_link, genre: genreList, story: episodeData.story, url: episodeData.url, runtime: episodeData.runtime, castList: cast, directors: director, startTime: wh.present? ? wh.duration : 0}
+                          {
+                            message: "MSG_SUCCESS", 
+                            status: 200, 
+                            videoDetails: detailsList,
+                            episodeList: episode,
+                            recommendationList: recommendation
+                          }
+                        end
+                      end
+                    else
+                      {message: "INVALID_USER", status: 500}
+                    end  
+        
+                  rescue Exception => e
+                    logger.info "API Exception-#{Time.now}-detailPage-#{params.inspect}-Error-#{e}"
+                    {message: "MSG_ERROR", status: 500}
+                  end
+                end
+              end
+
         end
     end
 end
