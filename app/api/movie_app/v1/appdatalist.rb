8727ce76c7d5c8c8f8304cb9ccd62f41c50b79ea
Anil Kumar <aniltimt@gmail.com>
2023-12-27 17:25:34 +0530

    more fix

--- app/api/movie_app/v1/appdatalist.rb
+++ app/api/movie_app/v1/appdatalist.rb
@@ -23,26 +23,32 @@ module MovieApp
               	  contents = Content.search(params[:searchTxt])
               	  if contents.present?
               	  	contents.each do |item|
+              	  	  if !item.title.blank?
               	  	  showHash = {id: item.id, banner: item.banner, contentName: item.title,
-              	  	   episodeName: item.title, duration: '120'}
+              	  	   episodeName: item.title, duration: '120 min'}
                       content_data << showHash
+                      end
               	  	end
               	  end
               	  episodes = Episode.search(params[:searchTxt])
               	  if episodes.present?
               	  	episodes.each do |item|
+              	  	  if !item.title.blank?	
               	  	  showHash = {id: item.id, banner: item.thumbnail, contentName: item.title,
               	  	   episodeName: item.title, duration: item.runtime}
                       content_data << showHash
+                      end
               	  	end
               	  end
               	  {message: MSG_SUCCESS, status: 200, showList: content_data}
               	else
               	  contents = Content.all.sample(10)
               	  contents.each do |item|
+              	  	if !item.title.blank?
               	  	showHash = {id: item.id, banner: item.banner, contentName: item.title,
-              	  	  episodeName: item.title, duration: '60'}
+              	  	  episodeName: item.title, duration: '60 min'}
                     content_data << showHash
+                    end
               	  end	
                   {message: MSG_SUCCESS, status: 200, showList: content_data}
               	end                
