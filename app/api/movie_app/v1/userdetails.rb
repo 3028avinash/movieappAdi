ac2b69852f09c08f61fa01f69a871c92a1515fc8
3028avinash <85918644+3028avinash@users.noreply.github.com>
2024-01-11 13:57:48 +0530

    minor chanegs in api

--- app/api/movie_app/v1/userdetails.rb
+++ app/api/movie_app/v1/userdetails.rb
@@ -559,7 +559,7 @@ module MovieApp
 
                   valid_subscription = Subscription.find_by(params[:subscriptionId])
 
-                  if valid_subscription.offer_amount.to_f == valid_payment.amount
+                  if valid_subscription.offer_amount.to_f == valid_payment.amount.to_f
 
                     duration = valid_subscription.duration.split(' ')
                     duration_number = duration[0].to_i
@@ -597,7 +597,7 @@ module MovieApp
 
                   valid_subscription = Subscription.find_by(params[:subscriptionId])
 
-                  if valid_subscription.offer_amount.to_f == valid_payement.amount
+                  if valid_subscription.offer_amount.to_f == valid_payement.amount.to_f
 
                     duration = valid_subscription.duration.split(' ')
                     duration_number = duration[0].to_i
